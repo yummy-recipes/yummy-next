b59187079ae6eaad4804e5c459b178e012c4512f
ertrzyiks <mateusz.derks@gmail.com>
2023-07-15 14:37:07 +0200

    Add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-import Image from 'next/image'
 import Link from 'next/link'
-import { Inter } from 'next/font/google'
-import Click from './click'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default async function Home() {
   const recipes = await prisma.recipe.findMany({
@@ -15,17 +11,8 @@ export default async function Home() {
     }
   })
 
-  const categories = await prisma.category.findMany()
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
-      {categories.map((category) => (
-        <div key={category.id} className="flex place-items-center">
-          <Link href={`/${category.slug}`}>{category.title}</Link>
-        </div>
-      ))}
-
-      <hr />
+    <main className="flex min-h-screen flex-col items-center justify-between">
 
       {recipes.map((recipe) => (
         <div key={recipe.id} className="flex place-items-center">
