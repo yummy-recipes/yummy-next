4baa3a6b1ceb496724de20cb514e97cd3d308979
ertrzyiks <mateusz.derks@gmail.com>
2023-09-25 21:52:26 +0200

    Add pictures to homepage

--- src/components/recipe-list-item/recipe-list-item.stories.tsx
+++ src/components/recipe-list-item/recipe-list-item.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { RecipeListItem } from './recipe-list-item';
 
+const imageUrl = 'https://res.cloudinary.com/dddjfvneq/image/fetch/f_auto,c_limit,w_800,q_auto/https://media.graphassets.com/bVmSWqhQOuOiBenDcvQO'
+
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'UI/RecipeListItem',
@@ -20,5 +22,6 @@ export const Primary: Story = {
   args: {
     href: '/',
     title: 'Spaghetti',
+    coverImage: imageUrl,
   },
 };
