52841a652ac2c1bb7bc13198a17d9bb59a8ec199
ertrzyiks <mateusz.derks@gmail.com>
2025-01-01 19:15:50 +0100

    Preserve search order

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,23 +1,31 @@
-import { PrismaClient } from '@prisma/client'
-import algoliasearch from 'algoliasearch'
+import { PrismaClient } from "@prisma/client";
+import algoliasearch from "algoliasearch";
 
-const prisma = new PrismaClient()
-const client = algoliasearch('J8YFF4CZ4C', process.env.ALGOLIA_SEARCH_KEY)
+const prisma = new PrismaClient();
+const client = algoliasearch("J8YFF4CZ4C", process.env.ALGOLIA_SEARCH_KEY);
 
-const index = client.initIndex(process.env.ALGOLIA_SEARCH_INDEX || 'prod_recipes')
+const index = client.initIndex(
+  process.env.ALGOLIA_SEARCH_INDEX || "prod_recipes"
+);
 
 export async function search(query: string) {
+  const { hits } = await index.search<{ slug: string }>(query);
 
-  const { hits } = await index.search<{ slug: string }>(query)
+  const slugs = hits.map((hit) => hit.slug);
 
   const recipes = await prisma.recipe.findMany({
     where: {
-      slug: { in: hits.map(hit => hit.slug) }
+      slug: { in: slugs },
     },
     include: {
       category: true,
-    }
-  })
+    },
+  });
 
   return recipes
+    .slice()
+    .sort(
+      (recipe1, recipe2) =>
+        slugs.indexOf(recipe1.slug) - slugs.indexOf(recipe2.slug)
+    );
 }
