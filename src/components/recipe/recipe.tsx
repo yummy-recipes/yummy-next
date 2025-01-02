27e45cf5f1119af4bfd8f5f6a8e1ba343cb92d69
ertrzyiks <mateusz.derks@gmail.com>
2025-01-02 14:56:29 +0100

    Improve recipe page

--- src/components/recipe/recipe.tsx
+++ src/components/recipe/recipe.tsx
@@ -1,33 +1,55 @@
-import Image from 'next/image'
-import { markdownToHtml } from '@/lib/markdown'
+import Image from "next/image";
+import { markdownToHtml } from "@/lib/markdown";
 
 interface Props {
-  title: string
-  coverImage: string
-  ingredients: { id: number, content: string }[]
-  instructions: { id: number, content: string }[]
+  title: string;
+  coverImage: string;
+  ingredients: { id: number; content: string }[];
+  instructions: { id: number; content: string }[];
 }
 
-export function Recipe({ title, coverImage, ingredients, instructions }: Props) {
+export function Recipe({
+  title,
+  coverImage,
+  ingredients,
+  instructions,
+}: Props) {
   return (
-    <div className='flex flex-col w-full'>
-      <div className='my-12'>
+    <div className="flex flex-col w-full">
+      <div className="my-12">
         <h1 className="text-gradient uppercase inline text-4xl">{title}</h1>
       </div>
 
-      <Image src={coverImage} width={40} height={30} alt={`Photo of ${title}`} />
+      <Image
+        src={coverImage}
+        width={40}
+        height={30}
+        alt={`Photo of ${title}`}
+      />
 
-      <div className="p-2 m-2">
-        {ingredients.map(ingredient => (
-          <div key={ingredient.id} dangerouslySetInnerHTML={{ __html: markdownToHtml(ingredient.content) }}></div>
-        ))}
-      </div>
+      <div className="flex flex-col md:flex-row">
+        <div className="flex-1 p-2 m-2">
+          {ingredients.map((ingredient) => (
+            <div
+              key={ingredient.id}
+              dangerouslySetInnerHTML={{
+                __html: markdownToHtml(ingredient.content),
+              }}
+            ></div>
+          ))}
+        </div>
 
-      <div className="p-2 m-2">
-        {instructions.map(instruction => (
-          <div key={instruction.id} dangerouslySetInnerHTML={{ __html: markdownToHtml(instruction.content) }}></div>
-        ))}
+        <div className="flex-1 p-2 m-2">
+          {instructions.map((instruction) => (
+            <div
+              key={instruction.id}
+              dangerouslySetInnerHTML={{
+                __html: markdownToHtml(instruction.content),
+              }}
+            ></div>
+          ))}
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,25 +1,33 @@
-import { marked } from 'marked'
+import { marked } from "marked";
 
 class TailwindRenderer extends marked.Renderer {
   heading(text: string, level: number) {
-    return `<h${level} class='text-2xl my-4'>${text}</h${level}>`
+    return `<h${level} class='text-2xl my-4'>${text}</h${level}>`;
+  }
+  paragraph(text: string): string {
+    return `<p class='my-2'>${text}</p>`;
   }
 
-  list(this: marked.Renderer<never> | marked.RendererThis, body: string, ordered: boolean, start: number): string {
+  list(
+    this: marked.Renderer<never> | marked.RendererThis,
+    body: string,
+    ordered: boolean,
+    start: number
+  ): string {
     if (ordered) {
-      return `<ol class='list-decimal list-inside'>${body}</ol>`
+      return `<ol class='list-decimal list-inside'>${body}</ol>`;
     }
 
-    return `<ul class='list-disc list-inside'>${body}</ul>`
+    return `<ul class='list-disc list-inside'>${body}</ul>`;
   }
 }
 
-const renderer = new TailwindRenderer()
+const renderer = new TailwindRenderer();
 
 export function markdownToHtml(markdown: string): string {
   return marked.parse(markdown, {
     mangle: false,
     headerIds: false,
-    renderer
-  })
+    renderer,
+  });
 }
