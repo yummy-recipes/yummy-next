45c5fc59e54f92d26ce78af97a8f7acc48d1fa31
ertrzyiks <mateusz.derks@gmail.com>
2025-11-13 20:54:21 +0100

    Load data from BE

--- src/app/api/init-statsig-values/route.ts
+++ src/app/api/init-statsig-values/route.ts
@@ -1,20 +1,47 @@
+import { cookies } from "next/headers";
+import Statsig, { type InitializationDetails } from "statsig-node";
+
+let statsigInitialization: Promise<InitializationDetails> | undefined =
+  undefined;
+
 export async function GET(request: Request) {
-  return new Response(JSON.stringify({ message: "Not implemented" }), {
-    status: 501,
-  });
-  // const stableID = request.cookies.get("s_stableID")?.value;
+  const cookieStore = await cookies();
+  const stableID = cookieStore.get("s_stableID")?.value;
 
-  // if (!stableID) {
-  //   return new Response(JSON.stringify({ error: "Stable ID not found" }), {
-  //     status: 404,
-  //   });
-  // }
+  if (!stableID) {
+    return new Response(JSON.stringify({ error: "Stable ID not found" }), {
+      status: 404,
+    });
+  }
 
-  // const bootstrapValues = await statsig.getClientInitializeResponse({
-  //   customIDs: { stableID },
-  // });
+  if (!statsigInitialization) {
+    statsigInitialization = Statsig.initialize(
+      process.env.STATSIG_SERVER_KEY!,
+      {
+        environment: { tier: process.env.NODE_ENV ?? "development" },
+      },
+    );
+  }
 
-  // return new Response(JSON.stringify({ stableID, bootstrapValues }), {
-  //   status: 200,
-  // });
+  await statsigInitialization;
+
+  const bootstrapValues = Statsig.getClientInitializeResponse(
+    {
+      customIDs: { stableID },
+    },
+    process.env.NEXT_PUBLIC_STATSIG_CLIENT_KEY!,
+    {
+      hash: "djb2",
+    },
+  );
+
+  return new Response(
+    JSON.stringify({
+      stableID,
+      bootstrapValues: JSON.stringify(bootstrapValues),
+    }),
+    {
+      status: 200,
+    },
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,8 @@ import { Exo } from "next/font/google";
 import "./globals.css";
 import "vanilla-cookieconsent/dist/cookieconsent.css";
 import { Search } from "@/components/search/search";
-import { cookies } from "next/headers";
 import { CookieBanner } from "@/components/cookie-banner/cookie-banner";
-import { StatsigBootstrapProvider } from "@statsig/next";
+import { StatsigProvider } from "@/components/statsig-provider/statsig-provider";
 
 const exo = Exo({ subsets: ["latin"] });
 
@@ -19,18 +18,10 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = await cookies();
-  const stableID = cookieStore.get("s_stableID")?.value || "anonymous";
-
   return (
     <html lang="en">
       <body className={exo.className}>
-        <StatsigBootstrapProvider
-          clientKey={process.env.NEXT_PUBLIC_STATSIG_CLIENT_KEY!}
-          serverKey={process.env.STATSIG_SERVER_KEY!}
-          user={{ customIDs: { stableID } }}
-          useCookie={false}
-        >
+        <StatsigProvider>
           <div className="max-w-7xl mx-auto px-4 md:px-8 2xl:px-0">
             <Header>
               <div className="w-full sm:w-96 max-w-sm">
@@ -44,7 +35,7 @@ export default async function RootLayout({
           </div>
 
           <CookieBanner />
-        </StatsigBootstrapProvider>
+        </StatsigProvider>
       </body>
     </html>
   );
